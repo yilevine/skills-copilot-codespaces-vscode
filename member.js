ed2385a436cb0cc95a550b09512e6d6b85cbd489
Yile <39296617+yilevine@users.noreply.github.com>
2023-08-07 20:10:32 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,19 +1,27 @@
-function skillsmember() {
-    var skills = document.getElementById("skills").value;
-    var skills = skills.split(",");
-    var skills = skills.map(function (item) {
-        return item.trim();
-    });
-    var skills = skills.filter(function (item) {
-        return item != "";
-    });
-    var skills = skills.map(function (item) {
-        return item.toUpperCase();
-    });
-    var skills = skills.sort();
-    var skills = skills.filter(function (item, pos) {
-        return skills.indexOf(item) == pos;
-    });
-    document.getElementById("skills").value = skills.join(", ");
-}
-    
+function skillsmember {
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    var member = {
+        name: "John",
+        age: 25,
+        skills: skills
+    };
+    document.write(member.skills[0]);
+    }       
+    skillsmember(); 
+    // Output: HTML
+```
+### 3. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+```javascript
+// Path: delete.js
+function deleteRollno {
+    var student = {
+        name : "David Rayy",
+        sclass : "VI",
+        rollno : 12 };
+    document.write(student);
+    delete student.rollno;
+    document.write(student);
+    }
+    deleteRollno();
+    // Output: Object { name: "David Rayy", sclass: "VI", rollno: 12 } Object { name: "David Rayy", sclass: "VI" }
+```
