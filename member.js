ad5667d446cad02e5b4c1bed39f0b3012ebb02ca
Yile <39296617+yilevine@users.noreply.github.com>
2023-08-08 20:59:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,80 @@
+function skillsMember() {
+    this.name = 'skillsMember';
+    this.id = '#skills-member';
+    this.data = {};
+    this.data.skills = [];
+    this.data.skills[0] = {
+        name: 'HTML5',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[1] = {
+        name: 'CSS3',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[2] = {
+        name: 'Javascript',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[3] = {
+        name: 'jQuery',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[4] = {
+        name: 'PHP',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[5] = {
+        name: 'MySQL',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[6] = {
+        name: 'Git',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[7] = {
+        name: 'Bootstrap',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[8] = {
+        name: 'AngularJS',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[9] = {
+        name: 'NodeJS',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[10] = {
+        name: 'MongoDB',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[11] = {
+        name: 'ExpressJS',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[12] = {
+        name: 'SailsJS',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[13] = {
+        name: 'ReactJS',
+        value: 80,
+        color: '#4caf50'
+    };
+    this.data.skills[14] = {
+        name: 'Wordpress',
+        value: 80,
+        color: '#4caf
+    };
